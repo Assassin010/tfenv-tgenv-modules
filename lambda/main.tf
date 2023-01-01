1b31cefd3faba65722fc7bd08a52e6f7689d9a5d
Assassin010 <gauthier.kwatatshey@gmail.com>
2023-01-01 21:55:27 +0100

    edit

--- lambda/main.tf
+++ lambda/main.tf
@@ -57,12 +57,12 @@ data "archive_file" "my_lambda" {
 }
 
 resource "aws_lambda_function" "test_lambda" {
-  filename      = "lambda_function.zip"
-  function_name = "kwatatshey"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.8"
-  filename = "${var.zip_output_dir}/lambda_function.zip" // <----- specify root dir as a var
-  source_code_hash  = "${data.archive_file.lambda_function.output_base64sha256}"
+  /*filename      = "lambda_function.zip"*/
+  function_name    = "kwatatshey"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.8"
+  filename         = "${var.zip_output_dir}/lambda_function.zip"
+  source_code_hash = data.archive_file.lambda_function.output_base64sha256
 }
-  /*source_code_hash = filebase64sha256("lambda_function.zip")*/
+/*source_code_hash = filebase64sha256("lambda_function.zip")*/
